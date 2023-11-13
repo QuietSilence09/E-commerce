bb5616b5484d8b181a2e7d12d618a3687cccb39e
QuietSilence09 <ndanik26@gmail.com>
2023-11-13 18:30:19 +0200

    billboards style changes

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -38,7 +38,7 @@ const ImageUpload: FC<ImageUploadProps> = ({
     <>
       <div className="mb-4 flex items-center gap-4">
         {value.map((url) =>(
-            <div key={url} className="realtive w-[200px] h-[200px] rounded-md overflow-hidden">
+            <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
                 <div className="z-10 absolute top-2 right-2">
                     <Button type="button" onClick={() => onRemove(url)} variant="destructive" size="icon">
                         <Trash className="h-4 w-4"/>
